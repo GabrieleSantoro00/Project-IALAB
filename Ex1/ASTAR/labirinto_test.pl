9b0a5fe15a761da5f4693ccd076fed95c351f3ab
Gabriele Santoro <gabriele.santoro956@edu.unito.it>
2024-07-09 11:27:04 +0200

    New final version of ASTAR (test passed but wrong path printed)

---
+++ Ex1/ASTAR/labirinto_test.pl
@@ -0,0 +1,15 @@
+num_colonne(3).
+num_righe(3).
+
+iniziale(pos(1,1)).
+finale(pos(3,3)).
+
+occupata(pos(1,2)).
+occupata(pos(2,2)).
+occupata(pos(2,3)).
+
+  | I  X  O |
+  | O  X  X |
+  | O  O  F |
+
+  
---
+++ Ex1/ASTAR/test_astar.pl
@@ -0,0 +1,55 @@
+:- ['azioni'], ['labirinto_test'].
+
+astar(Cammino) :-
+    iniziale(NodoIniziale),
+    finale(NodoFinale),
+    euristica(NodoIniziale, NodoFinale, H),
+    G is 0,
+    F is H + G,
+    astar_aux([(F, H, NodoIniziale, [])], [], NodoFinale, CamminoRovesciato),
+    inverti(CamminoRovesciato, Cammino).
+
+euristica(pos(R1, C1), pos(R2, C2), Valore) :-
+    DistanzaR is abs(R1 - R2),
+    DistanzaC is abs(C1 - C2),
+    Valore is DistanzaR + DistanzaC.
+
+astar_aux([( _, _, Nodo, Cammino)| _], _, NodoFinale, Cammino) :-finale(Nodo),!.
+astar_aux([(F, H, Nodo, Cammino)|Coda], Visitati, NodoFinale, CamminoFinale) :-
+    \+member(Nodo, Visitati),!,
+    findall(Azione, applicabile(Azione, Nodo), ListaAzioni),
+    generaNuoviStati(Nodo, ListaAzioni, ListaNuoviStati),
+    calcolaFNuoviStati(ListaNuoviStati, NodoFinale, ListaNuoviStatiConF),
+    inserisci_lista_ordinata(ListaNuoviStatiConF, Coda, NuovaCoda),
+    astar_aux(NuovaCoda, [Nodo | Visitati], NodoFinale, CamminoFinale).
+
+generaNuoviStati(_, [], []).
+generaNuoviStati(Nodo, [Azione | AzioniTail], [(NodoGenerato, [Azione | Cammino]) | NuoviStatiTail]) :-
+    trasforma(Azione, Nodo, NodoGenerato),
+    generaNuoviStati(Nodo, AzioniTail, NuoviStatiTail).
+
+calcolaFNuoviStati([], _, []).
+calcolaFNuoviStati([(Nodo, Cammino) | Coda], NodoFinale, [(F, H, Nodo, Cammino) | ListaNuoviStatiConF]) :-
+    euristica(Nodo, NodoFinale, H),
+    length(Cammino, G),
+    F is G + H,
+    calcolaFNuoviStati(Coda, NodoFinale, ListaNuoviStatiConF).
+
+inserisci_ordinato(E, [], [E]).
+inserisci_ordinato((F, H, Nodo, Cammino), [(F1, H1, Nodo1, Cammino1) | Coda], [(F, H, Nodo, Cammino), (F1, H1, Nodo1, Cammino1) | Coda]) :-
+    F =< F1.
+inserisci_ordinato((F, H, Nodo, Cammino), [(F1, H1, Nodo1, Cammino1) | Coda], [(F1, H1, Nodo1, Cammino1) | ListaOrdinata]) :-
+    F > F1,
+    inserisci_ordinato((F, H, Nodo, Cammino), Coda, ListaOrdinata).
+
+inserisci_lista_ordinata([], Lista, Lista).
+inserisci_lista_ordinata([E | Es], Lista, ListaOrdinata) :-
+    inserisci_ordinato(E, Lista, ListaTemp),
+    inserisci_lista_ordinata(Es, ListaTemp, ListaOrdinata).
+
+inverti(Lista, ListaInvertita) :-
+    inverti(Lista, [], ListaInvertita).
+
+inverti([], Accumulatore, Accumulatore).
+inverti([Testa|Coda], Accumulatore, ListaInvertita) :-
+    inverti(Coda, [Testa|Accumulatore], ListaInvertita).
