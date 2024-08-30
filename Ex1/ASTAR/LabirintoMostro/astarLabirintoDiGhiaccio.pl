f2ca08066d12ba83e885b825f50d5e2d60c792ac
Gabriele Santoro <gabriele.santoro956@edu.unito.it>
2024-08-30 17:15:14 +0200

    add statistics calls + comments in ice labyrinth astar

--- Ex1/ASTAR/LabirintoMostro/astarLabirintoDiGhiaccio.pl
+++ Ex1/ASTAR/LabirintoMostro/astarLabirintoDiGhiaccio.pl
@@ -1,27 +1,63 @@
-:- ['azioni'], ['labirinto.pl'], ['stampaLabirinto'].
+:- ['azioni.pl'], ['stampaLabirinto.pl'], ['../../labirinti/labirintoDiGhiaccio1.pl'].
 :- set_prolog_flag(answer_write_options, [max_depth(0)]).
 
-astar(Cammino) :-
-  esegui(),
+/**
+ * astar/0, fa da wrapper per l'inizio dell'esecuzione.
+ */
+
+astar() :-
+  statistics(cputime, Start),
   iniziale(StatoIniziale),
   finale(StatoFinale),
   euristica(StatoIniziale, StatoFinale, H),
   G is 0,
   F is H + G,
   astar_aux([(F, H, StatoIniziale, [])], [], StatoFinale, CamminoRovesciato),
-  inverti(CamminoRovesciato, Cammino).
+  statistics(cputime, Stop),
+  inverti(CamminoRovesciato, Cammino),
+  write("Nodo iniziale: "), writeln(StatoIniziale),
+  write("Nodo finale: "), writeln(StatoFinale),
+  write("Percorso: "), writeln(Cammino),
+  write("Tempo di esecuzione: "), T is Stop - Start, writeln(T),
+  write(""),
+  esegui().
 
+
+/**
+ * Predicato euristica/3, calcola la distanza tra un nodo e
+ * i nodi finali e restituisce il valore minimo.
+ */
 
 euristica((pos(R1, C1), _, _, _, _), pos(R2, C2), Valore) :-
   DistanzaR is abs(R1 - R2),
   DistanzaC is abs(C1 - C2),
   Valore is DistanzaR + DistanzaC.
 
+/**
+ * astar_aux(+Coda, +Visitati, +StatoFinale, -CamminoFinale)
+ *
+ * Predicato che implementa l'algoritmo di ricerca A* (A-star).
+ *
+ * Parametri:
+ * - Coda: Lista di nodi da esplorare, ciascuno rappresentato come una tupla (F, H, Stato, Cammino).
+ * - Visitati: Lista di nodi già visitati.
+ * - StatoFinale: Stato obiettivo.
+ * - CamminoFinale: Cammino trovato dal nodo iniziale al nodo finale.
+ *
+ * Il predicato funziona come segue:
+ * 1. Se il nodo corrente è lo stato finale (arrivoAlPortale/1), restituisce il cammino.
+ * 2. Altrimenti, genera i nuovi stati applicabili al nodo corrente.
+ * 3. Filtra i nuovi stati per rimuovere quelli già visitati.
+ * 4. Calcola i valori F per i nuovi stati.
+ * 5. Inserisce i nuovi stati nella coda in modo ordinato.
+ * 6. Aggiorna la lista dei nodi visitati.
+ * 7. Chiama ricorsivamente astar_aux/4 con la nuova coda e la lista aggiornata dei nodi visitati.
+ */
+
 
 astar_aux([( _, _, Stato, Cammino)| _], _, StatoFinale, Cammino) :-
-  writeln(''), writeln('Stato:'), writeln(Stato),
-  writeln(''), writeln('Cammino:'), writeln(Cammino),
   arrivoAlPortale(Stato),!.
+
 astar_aux([(F, H, Stato, Cammino)| Coda], Visitati, StatoFinale, CamminoFinale) :-
   generaNuoviStati((Stato, Cammino), ListaNuoviStati),
   differenza(ListaNuoviStati, Visitati, ListaNuoviStatiDaVisitare),
@@ -30,10 +66,16 @@ astar_aux([(F, H, Stato, Cammino)| Coda], Visitati, StatoFinale, CamminoFinale) 
   append(ListaNuoviStatiDaVisitare, Visitati, NuoviVisitati), 
   astar_aux(NuovaCoda, [(Stato, Cammino) | NuoviVisitati], StatoFinale, CamminoFinale).
 
+/**
+ * Predicato generaNuoviStati/2, genera i nuovi stati a partire dallo stato attuale.
+ */
 
 generaNuoviStati((Stato, Cammino), ListaNuoviStati) :-
   muovi_tutte_le_direzioni((Stato, Cammino), ListaNuoviStati).
 
+/**
+ * Predicato differenza/3, calcola la differenza tra due liste.
+ */
 
 differenza([], _, []).
 differenza([(Stato, Cammino) | Tail], Visitati, Risultato) :-
@@ -42,6 +84,9 @@ differenza([(Stato, Cammino) | Tail], Visitati, Risultato) :-
 differenza([(Stato, Cammino) | Tail], Visitati, [(Stato, Cammino) | RisTail]) :-
     differenza(Tail, Visitati, RisTail).
 
+/**
+ * Predicato calcolaFNuoviStati/3, calcola il valore di F per ogni nuovo stato.
+ */
 
 calcolaFNuoviStati([], _, []).
 calcolaFNuoviStati([(Stato, Cammino) | Coda], StatoFinale, [(F, H, Stato, Cammino) | ListaNuoviStatiConF]) :-
@@ -50,6 +95,10 @@ calcolaFNuoviStati([(Stato, Cammino) | Coda], StatoFinale, [(F, H, Stato, Cammin
   F is G + H,
   calcolaFNuoviStati(Coda, StatoFinale, ListaNuoviStatiConF).
 
+/**
+ * Predicato inserisci_ordinato/3, inserisce un elemento in una lista ordinata.
+ */
+
 inserisci_ordinato(E, [], [E]).
 inserisci_ordinato((F, H, Stato, Cammino), [(F1, H1, Stato1, Cammino1) | Coda], [(F, H, Stato, Cammino), (F1, H1, Stato1, Cammino1) | Coda]) :-
   F =< F1.
@@ -57,12 +106,18 @@ inserisci_ordinato((F, H, Stato, Cammino), [(F1, H1, Stato1, Cammino1) | Coda], 
   F > F1,
   inserisci_ordinato((F, H, Stato, Cammino), Coda, ListaOrdinata).
 
+/**
+ * Predicato inserisci_lista_ordinata/3, inserisce una lista di elementi in una lista ordinata.
+ */
 
 inserisci_lista_ordinata([], Lista, Lista).
 inserisci_lista_ordinata([E | Es], Lista, ListaOrdinata) :-
   inserisci_ordinato(E, Lista, ListaTemp),
   inserisci_lista_ordinata(Es, ListaTemp, ListaOrdinata).
 
+/**
+ * Predicato inverti/2, inverte una lista.
+ */
 
 inverti(Lista, ListaInvertita) :-
   inverti(Lista, [], ListaInvertita).
--- Ex1/ASTAR/LabirintoMostro/azioni.pl
+++ Ex1/ASTAR/LabirintoMostro/azioni.pl
@@ -7,14 +7,11 @@ iniziale(StatoIniziale) :-
     HaMartello = false,
     StatoIniziale = (Mostro, Gemme, BlocchiDiGhiaccio, Martello, HaMartello).
 
-
 % Predicato per l'acquisizione dello stato finale del labirinto
 finale(Mostro) :-
   portale(Mostro).
 
-
 arrivoAlPortale((Mostro, _, _, _, _)) :- finale(Mostro).
-
 
 % Definizione delle direzioni di movimento
 direzione(nord, pos(R, C), pos(R2, C)) :- R2 is R - 1, R2 > 0.
@@ -22,48 +19,34 @@ direzione(sud, pos(R, C), pos(R2, C)) :- num_righe(NR), R2 is R + 1, R2 =< NR.
 direzione(ovest, pos(R, C), pos(R, C2)) :- C2 is C - 1, C2 > 0.
 direzione(est, pos(R, C), pos(R, C2)) :- num_colonne(NC), C2 is C + 1, C2 =< NC.
 
-
 % Predicati che estraggono le posizioni dei vari oggetti del labirinto dalla tupla Stato attuale.
-
 
 posizione_mostro((Mostro, _, _, _, _), Mostro).
 
-
 posizione_gemme((_, Gemme, _, _, _), Gemme).
-
 
 posizione_martello((_, _, _, Martello, _), Martello).
 
-
 posizione_blocco_di_ghiaccio((_, _, BlocchiDiGhiaccio, _, _), BlocchiDiGhiaccio).
-
 
 ha_martello((_, _, _, _, HaMartello), HaMartello).
 
-
 muovi_tutte_le_direzioni((StatoIniziale, Cammino), ListaStatiFinali) :-
   muovi_tutti_gli_oggetti(nord, StatoIniziale, StatoFinaleNord),
-  %writeln("StatoFinaleNord: "), writeln(StatoFinaleNord),
   muovi_tutti_gli_oggetti(sud, StatoIniziale, StatoFinaleSud),
-  %writeln("StatoFinaleSud: "), writeln(StatoFinaleSud),
   muovi_tutti_gli_oggetti(ovest, StatoIniziale, StatoFinaleOvest),
-  %writeln("StatoFinaleOvest: "), writeln(StatoFinaleOvest),
   muovi_tutti_gli_oggetti(est, StatoIniziale, StatoFinaleEst),
-  %writeln("StatoFinaleEst: "), writeln(StatoFinaleEst),
   ListaStatiFinali = [(StatoFinaleNord, [nord | Cammino]), (StatoFinaleSud, [sud | Cammino]), (StatoFinaleOvest, [ovest | Cammino]), (StatoFinaleEst, [est | Cammino])].
-
 
 % Predicato principale per muovere tutti gli oggetti nella direzione specificata
 muovi_tutti_gli_oggetti(Direzione, StatoIniziale, StatoFinale) :-
     ottieni_oggetti_mobili(StatoIniziale, OggettiMobili),
     muovi_oggetto_in_direzione(Direzione, OggettiMobili, StatoIniziale, StatoFinale).
 
-
 % Ottieni tutti gli oggetti mobili nel gioco
 ottieni_oggetti_mobili(Stato, (Mostro, Gemme)) :-
     posizione_mostro(Stato, Mostro),
     posizione_gemme(Stato, Gemme).
-
 
 muovi_oggetto_in_direzione(Direzione, (Mostro, Gemme), StatoIniziale, StatoFinale) :-
     muovi_gemme(Direzione, Gemme, StatoIniziale, StatoIntermedio),
@@ -73,7 +56,6 @@ muovi_oggetto_in_direzione(Direzione, (Mostro, Gemme), StatoIniziale, StatoFinal
         StatoFinale = StatoIntermedio
     ).
 
-
 muovi_gemme(_, [], Stato, Stato).
 muovi_gemme(Direzione, [Gemma|AltreGemme], StatoIniziale, StatoFinale) :-
     (muovi_gemma(Direzione, Gemma, StatoIniziale, StatoIntermedio) ->
@@ -82,10 +64,8 @@ muovi_gemme(Direzione, [Gemma|AltreGemme], StatoIniziale, StatoFinale) :-
         muovi_gemme(Direzione, AltreGemme, StatoIniziale, StatoFinale)
     ).
 
-
 %muovi_gemma(_, _, Stato, Stato). % Caso base: se non è possibile muovere ulteriormente la gemma, restituisci lo stato attuale
 muovi_gemma(Direzione, PosizioneCorrente, StatoIniziale, StatoFinale) :-
-    %write('Muovo la gemma da '), write(PosizioneCorrente), write(' in direzione '), writeln(Direzione),
     (direzione(Direzione, PosizioneCorrente, NuovaPos),
         posizione_valida(NuovaPos, StatoIniziale) ->
         aggiorna_stato(PosizioneCorrente, NuovaPos, StatoIniziale, StatoIntermedio),
@@ -94,13 +74,11 @@ muovi_gemma(Direzione, PosizioneCorrente, StatoIniziale, StatoFinale) :-
         StatoFinale = StatoIniziale
     ).
 
-
 posizione_valida(Posizione, (Mostro, Gemme, BlocchiDiGhiaccio, Martello, HaMartello)) :-
     Posizione \= Mostro,
     \+memberchk(Posizione, BlocchiDiGhiaccio),
     \+occupata(Posizione),
     \+memberchk(Posizione, Gemme).
-
 
 posizione_valida(pos(R, C)) :-
     num_righe(NR), num_colonne(NC),
@@ -108,16 +86,13 @@ posizione_valida(pos(R, C)) :-
     C > 0, C =< NC,
     \+occupata(pos(R, C)).
 
-
 aggiorna_stato(PosizioneCorrente, NuovaPos, (Mostro, Gemme, BlocchiDiGhiaccio, Martello, HaMartello), StatoFinale):-
     select(PosizioneCorrente, Gemme, GemmeSenzaCorrente), % Rimuove la posizione corrente dalla lista delle gemme
     StatoFinale = (Mostro, [NuovaPos|GemmeSenzaCorrente], BlocchiDiGhiaccio, Martello, HaMartello). % Aggiunge la nuova posizione alla lista delle gemme
 
-
 %muovi_mostro(_, _, Stato, Stato):- !.
 % Predicato principale per muovere il mostro
 muovi_mostro(Direzione, PosizioneCorrente, StatoIniziale, StatoFinale) :-
-  %write('Muovo il mostro da '), write(PosizioneCorrente), write(' in direzione '), writeln(Direzione),
   (direzione(Direzione, PosizioneCorrente, NuovaPos),
       posizione_valida(NuovaPos, StatoIniziale) ->
       (aggiorna_stato_mostro(PosizioneCorrente, NuovaPos, StatoIniziale, StatoIntermedio),
@@ -125,7 +100,6 @@ muovi_mostro(Direzione, PosizioneCorrente, StatoIniziale, StatoFinale) :-
   ;
       StatoFinale = StatoIniziale
   ).
-
 
 % Predicato per aggiornare lo stato del mostro
 aggiorna_stato_mostro(PosizioneCorrente, NuovaPos, (Mostro, Gemme, BlocchiDiGhiaccio, Martello, HaMartello), StatoFinale) :-
@@ -142,7 +116,6 @@ aggiorna_stato_mostro(PosizioneCorrente, NuovaPos, (Mostro, Gemme, BlocchiDiGhia
       NuovoStato = (NuovaPos, Gemme, BlocchiDiGhiaccio, Martello, NuovoHaMartello)
   ),
   StatoFinale = NuovoStato.
-
 
 % Predicato per rompere il ghiaccio
 rompi_ghiaccio(Posizione, BlocchiDiGhiaccio, NuoviBlocchiDiGhiaccio) :-
